e5399afd3afcb35e3c517621c15b786da23e728b
Michael Lawrence <mike@pandemicode.com>
2015-07-23 22:06:12 -0400

    Updating jsdoc comments. Started building SwatchList.

--- examples/simple/js/app.js
+++ examples/simple/js/app.js
@@ -14,7 +14,25 @@ let $colorPicker = document.getElementById('colorPicker'),
 	colorPicker = new ColorPicker(320, 320),
 	huePicker = new HuePicker(20, 320),
 	swatch = new Swatch(20, 20),
-	swatches = new SwatchList($swatches),
+	swatches = new SwatchList(20, 20, [
+		'#ff0000',
+		'#ffff00',
+		'#00ff00',
+		'#00ffff',
+		'#0000ff',
+		'#ff00ff',
+		'hsl(0, 0%, 100%)',
+		'hsl(0, 0%, 90%)',
+		'hsl(0, 0%, 80%)',
+		'hsl(0, 0%, 70%)',
+		'hsl(0, 0%, 60%)',
+		'hsl(0, 0%, 50%)',
+		'hsl(0, 0%, 40%)',
+		'hsl(0, 0%, 30%)',
+		'hsl(0, 0%, 20%)',
+		'hsl(0, 0%, 10%)',
+		'hsl(0, 0%, 0%)'
+	]),
 	customSwatches = new SwatchList($customSwatches);
 
 colorPicker.on(ColorPicker.CHANGE, function (color) {
@@ -25,6 +43,13 @@ huePicker.on(HuePicker.CHANGE, function (color) {
 	colorPicker.hue = color;
 });
 
+swatches.on(SwatchList.CHANGE, function (color) {
+	colorPicker.hue =
+		huePicker.color =
+			swatch.color = color;
+});
+
+swatches.appendTo($swatches);
 colorPicker.appendTo($colorPicker);
 huePicker.appendTo($huePicker);
 swatch.appendTo($swatch);
